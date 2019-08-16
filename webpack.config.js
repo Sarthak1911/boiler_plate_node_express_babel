f69d7186995ef5aec41b26e65da4214b1b901fb0
Sarthak Chavan <sarthakchavan290195@gmail.com>
2019-08-15 20:40:36 -0400

    All packages in

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require("path");
+
+module.exports = {
+  entry: "./src/js/app.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/dist"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"]
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
